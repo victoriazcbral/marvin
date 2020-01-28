110204dccc5fcd3a54ba2864b632ac68ca1995a0
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-01-28 09:34:33 -0300

    tres primeira

--- fase02/ex03/maiusculo.js
+++ fase02/ex03/maiusculo.js
@@ -1,5 +1,7 @@
-function maiusculo(palavra){ 
-    const palavras = palavra 
+function maiusculo(palavra){
+    const palavras = palavra
     return palavra.substring(0,3).toUpperCase().concat(palavra.substring(3))
 }
+
+
 

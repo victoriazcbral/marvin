0049dbc4c1116f8334093a120dad02fd7ce10346
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-02-04 11:57:58 -0300

    refazer 4

---
+++ fase03/ex04/tempo.js
@@ -0,0 +1,19 @@
+function tempo(data1, data2, unidade){
+    
+    var data = new Date(data1) 
+    var numb = new Date(data2)
+    switch (unidade){
+    case "dia";
+    var periodo = 
+    1000*60*60*24
+    break;
+    
+    
+    var decorrido = data.getTime() - numb.getTime(); 
+    console.log(decorrido)
+    
+
+    
+}
+
+

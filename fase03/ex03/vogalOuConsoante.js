52aadbf57090fd108df0279bd42bb9dcaced8561
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-02-04 08:39:58 -0300

    vogalouconsoante1

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,9 @@
+function VogalOuConsoante(letra){
+    
+if(letra==='a'|| letra==='e' || letra==='i' || letra==='o' || letra==='u'||letra==='A'|| letra==='E' || letra==='I' || letra==='O' || letra==='U'){
+     return ("vogal")
+ } else {
+     return ("consoante")
+ }
+ }
+ 

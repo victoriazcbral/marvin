516af91cc09afc9f31a0cd4b61a7bf3254586174
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-02-05 11:35:18 -0300

    fase3, exer4

--- fase03/ex04/tempo.js
+++ fase03/ex04/tempo.js
@@ -1,19 +1,21 @@
 function tempo(data1, data2, unidade){
-    
-    var data = new Date(data1) 
-    var numb = new Date(data2)
+    var dataIni = new Date (data1);
+    var dataFim = new Date (data2);
     switch (unidade){
-    case "dia";
-    var periodo = 
-    1000*60*60*24
-    break;
-    
-    
-    var decorrido = data.getTime() - numb.getTime(); 
-    console.log(decorrido)
-    
-
-    
+    case "dia":
+        var periodo = Math.abs(dataIni.getTime() - 
+        dataFim.getTime()) /1000/60/60/24;
+        return Math.round(periodo);
+        break;
+        case "mês":
+            var periodo = Math.abs(dataIni.getTime() - 
+            dataFim.getTime())/1000/60/60/24/30;
+            return Math.round(periodo);
+            default:
+            var periodo = Math.abs(dataIni.getTime() - 
+            dataFim.getTime())/1000/60/60/24/30/12;
+            return Math.round(periodo);
+            break;
+            
 }
-
-
+}

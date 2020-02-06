7222761ba73029dedddf271078d58c2a067bdcf3
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-02-06 08:38:00 -0300

    fase3exer5

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,20 @@
+function acharNumero(numero){
+    
+if(numero <5){
+     return ("mercurio")
+ }
+
+ if(numero >= 5 && numero <= 10){
+     return ("venus")
+ }
+ 
+  if(numero >= 10 && numero <= 100){
+     return ("terra")
+ }
+ 
+  if(numero > 100){
+     return ("marte")
+  }
+  
+ }
+ 

2936f98be782f0b7b10d2223deff10f910ef9ddd
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-02-06 11:10:11 -0300

    fase4, exer1

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,18 @@
+function ateZero(num){
+    
+    var c = 0
+    if(num > 0){
+    while(c <= num){
+    console.log(c)
+    c++
+}
+}
+
+   if(num <= 0){
+   while(c >= num){
+   console.log(num)
+   num++
+}
+}
+
+}

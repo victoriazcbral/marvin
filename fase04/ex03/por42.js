fbdf35babbb42b5ac889cd96d78b130c5fbd5a02
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-02-11 11:55:58 -0300

    aaa

---
+++ fase04/ex03/por42.js
@@ -0,0 +1,12 @@
+function por42(num1, num2){
+    
+	var a = null
+	while (a <= num2)
+	if (a % 42 === 0)
+	{Math.random()* 100
+	} else {
+	    return false;
+	    console.log(" Não encontrado ")
+	}
+}
+

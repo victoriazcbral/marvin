8691c9ba9543784c7996430b4c43fc6c862e7b30
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-02-13 09:38:49 -0300

    4, 3

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,12 +1,17 @@
 function por42(num1, num2){
     
-	var a = null
-	while (a <= num2)
-	if (a % 42 === 0)
-	{Math.random()* 100
-	} else {
-	    return false;
-	    console.log(" Não encontrado ")
+    var result = null;
+    var contador = 0;
+	while (num1 <= num2){
+	if (num1 % 42 === 0){
+	    contador = contador + 1;
+	    
 	}
+	if (contador === 2){
+	    return(num1);
+	}
+	  num1++  
+	}if (!result)
+	console.log("não encontrado")
+	return false
 }
-

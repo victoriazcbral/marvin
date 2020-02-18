84fe4bd83ff8e8a15a54dd6218e3838c7cdaea49
victoriazcbral <59872346+victoriazcbral@users.noreply.github.com>
2020-02-18 09:04:33 -0300

    fase 4, exer2

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -1,24 +1,19 @@
 function fizzbuzz(num1, num2){
+    
     var a = num1
     while (a <= num2){
         if ( a % 3 === 0 && a  % 5 === 0){
             console.log (a + " FizzBuzz ")
-         
-        }
-        if (a % 5 === 0){
+            
+        } else if (a % 5 === 0){
             console.log(a + " Buzz ")
-       
-        }
-        if (a % 3 === 0){
+            
+        } else if (a % 3 === 0){
             console.log(a + " Fizz ")
-       
+            
+        } else {
+        console.log(a)
         }
-        
-        if ( a % 3 != 0 && a  % 5 != 0){
-            console.log (a)
-       
-    }
-     a++
-    }  
+    a++
 }
-
+}
